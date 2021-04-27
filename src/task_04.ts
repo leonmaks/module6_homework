191c6deffa409b8eb2db8009ba6f83a7713c1aad
Le Ma <le_ma_@mail.ru>
2021-04-27 06:36:33 +0300

    minor changes

--- src/task_04.ts
+++ src/task_04.ts
@@ -2,6 +2,8 @@
 Напишите функцию, которая принимает два числа. Каждую секунду необходимо выводить в консоль, начиная от первого и заканчивая вторым. Используйте setInterval.
 Например, пользователь ввёл числа 5 и 15. Каждую секунду в консоль должно печататься число, начиная с 5 и заканчивая 15 (всего 11 чисел: 5 6 7 8 9 10 11 12 13 14 15). */
 
+const out = process.stdout.write.bind(process.stdout)
+
 const
   BEGIN = 5,
   END = 15
@@ -9,10 +11,10 @@ const
 let current = BEGIN
 
 const interval = setInterval(function () {
-  if (current > BEGIN) process.stdout.write(" ")
-  process.stdout.write(`${current}`)
+  if (current > BEGIN) out(" ")
+  out(`${current}`)
   if (++current > END) {
     clearInterval(interval)
-    process.stdout.write('\n')
+    out('\n')
   }
 }, 1000)
